ea42176ddc0d75f8f4099422d9d26a2a109e7532
dammyreginal <dami.okoro@gmail.com>
2022-06-05 18:37:14 -0700

    new commited file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 * Your program should end with the value 0
 */
 
-int main("Programming is like building a multilingual puzzle\n")
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
