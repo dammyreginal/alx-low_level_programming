80ad3e43ec414afdc2bc0c3994f079c0c7b150bb
dammyreginal <dami.okoro@gmail.com>
2022-06-11 09:50:08 +0000

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Prints the numbers since 0 to 9
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char c;
+for (c='0'; c<='9'; c++)
+{
+putchar(c);
+}
+
+putchar(\c);
+  
+return(0);
+}
