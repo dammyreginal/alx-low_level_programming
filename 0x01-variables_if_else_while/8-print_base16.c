7cd824433182b43dcf5b4c2fc1259a432a3bdc8c
dammyreginal <dami.okoro@gmail.com>
2022-06-12 21:00:22 +0000

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - prints a hexadecimal string.
+*
+*Return Aways (0)
+*/
+int main(void)
+{ char n;
+ 
+for (c = '0'; c <= '9' c++)
+ 
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar(\n);
+
+print (0);
+}
