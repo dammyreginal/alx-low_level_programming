ff96dbc4c02c0faca0583866955c83b55d63d3a6
dammyreginal <dami.okoro@gmail.com>
2022-06-13 06:53:34 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,8 @@ for (j = 0; j < 60; j++)
 {
 if (i < 10)
 {
-_putchar('0);
-_putchar(i + '0')
+_putchar('0');
+_putchar(i + '0');
 }
 else if(i >= 10)
 {
@@ -19,13 +19,14 @@ _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
 }
 if (j < 10)
+{
 _putchar(':');
 _putchar('0');
 _putchar(j + '0');
 }
 else if (j > 10)
 {
-_putchar(:);
+_putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 }
